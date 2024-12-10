9be43fac4a9f52ed285d06d6655960a11ceb45f1
DTheLegend <me@daudi.dev>
2024-12-10 23:55:18 +0000

    Getting Pretty Close

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,10 +1,12 @@
+use std::iter::repeat;
 use frunk::{Generic, HCons, HList, HNil};
 use frunk::prelude::*;
 use frunk_core::coproduct::CoproductSubsetter;
-use frunk_core::hlist::Sculptor;
-use frunk_core::traits::ToMut;
+use frunk_core::hlist::{Sculptor, HFoldLeftable, HMappable};
+use frunk_core::traits::{Func, Poly, ToMut};
 use nalgebra::{Quaternion, SVector};
 use rayon::prelude::*;
+use crate::ecs::example::Transform;
 
 trait Sealed {}
 impl Sealed for HNil {}
@@ -20,21 +22,21 @@ impl From<Entity> for usize {
 // Component lists use a modified HList pattern
 pub type ComponentStorage<T> = Vec<T>;
 
-pub trait StorageList: HList + Sealed {}
+pub trait ComponentList: HList + Sealed {}
 
-impl StorageList for HNil {}
-impl <HeadT, TailT: StorageList> StorageList for HCons<ComponentStorage<HeadT>, TailT> {}
+impl ComponentList for HNil {}
+impl <HeadT, TailT: ComponentList> ComponentList for HCons<ComponentStorage<HeadT>, TailT> {}
 
-pub trait ComponentList : HList + Sealed {
-    type StorageList: StorageList;
+trait ToComponentList {
+    type Output: HList;
 }
 
-impl ComponentList for HNil {
-    type StorageList = HNil;
+impl ToComponentList for HNil {
+    type Output = HNil;
 }
 
-impl <T, TailT : ComponentList> ComponentList for HCons<T, TailT> {
-    type StorageList = HCons<ComponentStorage<T>, TailT::StorageList>;
+impl <HeadT, TailT: ToComponentList> ToComponentList for HCons<HeadT, TailT> {
+    type Output = HCons<HeadT, <TailT as ToComponentList>::Output>;
 }
 
 pub struct Archetype<ComponentListT: ComponentList, EntityT : Into<usize> = Entity> {
@@ -42,47 +44,103 @@ pub struct Archetype<ComponentListT: ComponentList, EntityT : Into<usize> = Enti
     components: ComponentListT
 }
 
-impl <ComponentListT: ComponentList, EntityT : Into<usize>> Archetype<ComponentListT, EntityT> {
-}
-
 trait IntoArchetype : Generic<Repr: ComponentList> {
     type AsArchetype = Archetype<<Self as Generic>::Repr>;
 }
 
-
-trait ArchetypeList: Sealed {}
+trait ArchetypeList: Sealed {
+}
 
 impl ArchetypeList for HNil {}
 
 impl <T: ComponentList, TailT: ArchetypeList> ArchetypeList for HCons<Archetype<T>, TailT> {}
 
+// trait ComponentListZippable : ComponentList {
+//     type ZipT: HList;
+//     fn zip<'a>(&'a mut self) -> impl ParallelIterator<Item = Self::ZipT>;
+// }
+//
+// impl ComponentListZippable for HNil {
+//     type ZipT = HNil;
+//     fn zip<'a>(&'a mut self) -> impl ParallelIterator<Item = Self::ZipT> {
+//         repeat(HNil).par_bridge()
+//     }
+// }
+//
+// impl <HeadT, TailT: ComponentListZippable> ComponentListZippable for HCons<ComponentStorage<HeadT>, TailT>
+// where ComponentStorage<HeadT>: for<'a> IntoParallelRefMutIterator<'a, Iter: IndexedParallelIterator>,
+//     // TailT::ZipT: IntoParallelIterator<Iter: IndexedParallelIterator>
+// {
+//     type ZipT = HCons<HeadT, TailT::ZipT>;
+//
+//     fn zip<'a>(&'a mut self) -> impl ParallelIterator<Item=Self::ZipT> {
+//         self.head.par_iter_mut()
+//             .zip(ComponentListZippable::zip(&mut self.tail))
+//             .map(|(head, tail)| HCons::)
+//     }
+// }
+
 // SYSTEM
 trait System {
-    type Components: ComponentList;
-
-    fn instance_update(instance: Self::Components) -> Self::Components;
+    type InstanceT: ComponentList;
+    fn update_instance(instance: Self::InstanceT) -> Self::InstanceT;
 }
 
 pub trait SystemList: Sealed {
 }
 
-impl SystemList for HNil {}
-
-impl <T: System, TailT: SystemList> SystemList for HCons<T, TailT> {}
-
-// WORLD
-struct World<ArchetypeListT: ArchetypeList, SystemListT: SystemList> {
-    archetype_list: ArchetypeListT,
-    system_list: SystemListT
+impl SystemList for HNil {
 }
 
-impl <'a, SystemListT: SystemList, ArchetypeListT: ArchetypeList> World<ArchetypeListT, SystemListT> {
-    
+impl <T: System, TailT: SystemList> SystemList for HCons<T, TailT>{
+}
+
+struct World<ArchetypeListT: ArchetypeList, SystemListT: SystemList> {
+    archetypes: ArchetypeListT,
+    systems: SystemListT,
+}
+
+struct ParallelArrayMapping;
+
+impl <'a, T> Func<&'a mut ComponentStorage<T>> for ParallelArrayMapping
+where ComponentStorage<T>: IntoParallelRefMutIterator<'a, Iter: IndexedParallelIterator>
+{
+    type Output = impl IndexedParallelIterator;
+
+    fn call(i: &'a mut ComponentStorage<T>) -> Self::Output {
+        i.par_iter_mut()
+    }
+}
+
+struct ParallelArrayZip;
+
+impl <AccT: IndexedParallelIterator<Item: HList>, InputT: IndexedParallelIterator> Func<(AccT, InputT)> for ParallelArrayZip {
+    type Output = impl IndexedParallelIterator<Item: HList>;
+
+    fn call((acc, input): (AccT, InputT)) -> Self::Output {
+        acc.zip(input).map(|(acc, input)| acc.prepend(input))
+    }
+}
+
+impl <ArchetypeListT, SystemListT> System<ArchetypeListT, SystemListT>
+where
+    SystemListT: SystemList,
+
+    ArchetypeListT: ArchetypeList + for<'a> ToMut<'a>,
+    for<'a> <ArchetypeListT as ToMut<'a>>::Output: HMappable<Poly<ParallelArrayMapping>>,
+    for<'a> <<ArchetypeListT as ToMut<'a>>::Output as HMappable<Poly<ParallelArrayMapping>>>::Output: HFoldLeftable<Poly<ParallelArrayZip>, rayon::iter::Repeat<HNil>>,
+    for<'a> <<<ArchetypeListT as ToMut<'a>>::Output as HMappable<Poly<ParallelArrayMapping>>>::Output as HFoldLeftable<Poly<ParallelArrayZip>, rayon::iter::Repeat<HNil>>>::Output: ParallelIterator<Item = >
+{
+    fn apply_all(&mut self) {
+        let par_arrays = self.archetypes.to_mut().map(Poly(ParallelArrayMapping));
+        let zipped_par_arrays = par_arrays.foldl(Poly(ParallelArrayZip), rayon::iter::repeat(HNil));
+        
+        zipped_par_arrays.
+    }
 }
 
 mod example {
     use std::ops::{Add, AddAssign};
-    use frunk::prelude::*;
     use super::*;
 
     #[derive(Copy, Clone)]
@@ -135,23 +193,23 @@ mod example {
     }
 
     struct MovementSystem;
-    impl System for MovementSystem {
-        type Components = HList!(Transform, DeltaTransform);
-
-        fn update_instance(mut instance: Self::Components) -> Self::Components {
-            let delta = *instance.get::<DeltaTransform, _>();
-            *instance.get_mut::<Transform, _>() += delta;
-
-            instance
-        }
-    }
+    // impl System for MovementSystem {
+    //     type Components = HList!(Transform, DeltaTransform);
+    // 
+    //     fn update_instance(mut instance: Self::Components) -> Self::Components {
+    //         let delta = *instance.get::<DeltaTransform, _>();
+    //         *instance.get_mut::<Transform, _>() += delta;
+    // 
+    //         instance
+    //     }
+    // }
 
     struct RenderSystem;
-    impl System for RenderSystem {
-        type Components = HList!(Transform, Model);
-
-        fn update_instance(instance: Self::Components) -> Self::Components {
-            todo!()
-        }
-    }
+    // impl System for RenderSystem {
+    //     type Components = HList!(Transform, Model);
+    // 
+    //     fn update_instance(instance: Self::Components) -> Self::Components {
+    //         todo!()
+    //     }
+    // }
 }
